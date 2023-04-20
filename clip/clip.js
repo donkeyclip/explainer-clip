191b3ecd4dde838f7e7a3049ebafcc30727c1396
ivanvl01 <ivan.v@kissmybutton.gr>
2023-04-20 14:26:57 +0300

    fix: css core

--- clip/clip.js
+++ clip/clip.js
@@ -1,10 +1,12 @@
-import { HTMLClip } from "@donkeyclip/motorcortex";
+import { HTMLClip, setCSSCore } from "@donkeyclip/motorcortex";
 import html from "./clip.html";
 import css from "./clip.css";
 import initParams from "./initParams";
 import initParamsValidationRules from "./initParamsValidationRules";
 import { leftMove } from "./library/leftMove";
 import { opacity } from "./library/opacity";
+import AnimePluginDefinition from "@donkeyclip/motorcortex-anime";
+setCSSCore(AnimePluginDefinition.CSSEffect);
 
 export const clip = new HTMLClip({
   html,
@@ -15,7 +17,7 @@ export const clip = new HTMLClip({
   fonts: [
     {
       type: "google-font",
-      src: "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+      src: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
     },
   ],
   containerParams: {
@@ -24,11 +26,11 @@ export const clip = new HTMLClip({
   },
 });
 
-clip.addIncident(opacity(1,".text",1000),300);
-clip.addIncident(leftMove("-10px",".box:nth-child(2)",500),1500);
-clip.addIncident(leftMove("-20px",".box:nth-child(3)",500),1500);
-clip.addIncident(leftMove("0px",".box",500),3000);
-clip.addIncident(leftMove("362px",".box:nth-child(1)",800),4000);
-clip.addIncident(leftMove("242px",".box:nth-child(2)",800),4000);
-clip.addIncident(leftMove("122px",".box:nth-child(3)",800),4000);
-clip.addIncident(leftMove("83%",".text",800),4000);
+clip.addIncident(opacity(1, ".text", 1000), 300);
+clip.addIncident(leftMove("-10px", ".box:nth-child(2)", 500), 1500);
+clip.addIncident(leftMove("-20px", ".box:nth-child(3)", 500), 1500);
+clip.addIncident(leftMove("0px", ".box", 500), 3000);
+clip.addIncident(leftMove("362px", ".box:nth-child(1)", 800), 4000);
+clip.addIncident(leftMove("242px", ".box:nth-child(2)", 800), 4000);
+clip.addIncident(leftMove("122px", ".box:nth-child(3)", 800), 4000);
+clip.addIncident(leftMove("83%", ".text", 800, "linear", "50%"), 4000);
